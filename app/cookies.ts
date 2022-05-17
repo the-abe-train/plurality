b0a3a7285e1e064eefd8a4087aae1db0be1df83e
the-abe-train <abraham.train@gmail.com>
2022-05-16 22:37:00 -0400

    Fixed domain logic

--- app/cookies.ts
+++ app/cookies.ts
@@ -1,6 +1,12 @@
 import { createCookie } from "@remix-run/node";
 import { COOKIE_SIGNATURE, ROOT_DOMAIN } from "./util/env";
 
+const getDomain = () => {
+  if (process.env.NODE_ENV === "production") return "";
+  const url = new URL(ROOT_DOMAIN);
+  return url.hostname;
+};
+
 export const userCookie = createCookie("user", {
   maxAge: 604_800, // one week
   sameSite: "strict",
@@ -8,5 +14,5 @@ export const userCookie = createCookie("user", {
   secure: process.env.NODE_ENV === "production",
   expires: new Date(Date.now() + 60_000),
   secrets: [COOKIE_SIGNATURE],
-  domain: process.env.NODE_ENV === "production" ? ROOT_DOMAIN : "",
+  domain: getDomain(),
 });
