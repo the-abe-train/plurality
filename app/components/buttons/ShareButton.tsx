b1d4927ac47fb48d51d6eca366319953abfd5e22
the-abe-train <abraham.train@gmail.com>
2022-05-25 18:12:47 -0400

    Centered text

--- app/components/buttons/ShareButton.tsx
+++ app/components/buttons/ShareButton.tsx
@@ -58,7 +58,7 @@ https://plurality.fun`;
       >
         <img src={shareIcon} width={16} height={16} alt="Share icon" />
       </button>
-      <p>{msg}</p>
+      <p className="text-center">{msg}</p>
     </div>
   );
 }
