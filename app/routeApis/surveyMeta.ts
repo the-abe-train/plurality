36545c123ebe9b397d049b53b8dbcaaab045c134
the-abe-train <abraham.train@gmail.com>
2022-05-31 21:37:15 -0400

    Changed sharing data

--- app/routeApis/surveyMeta.ts
+++ app/routeApis/surveyMeta.ts
@@ -13,13 +13,13 @@ export const surveyMeta: MetaFunction = ({ data }: { data: LoaderData }) => {
   }
   return {
     title: `Plurality Survey #${data.survey._id}`,
-    description: `Plurality Survey #${data.survey._id}: ${data.survey.text}`,
+    description: data.survey.text,
     "twitter:image": `https://source.unsplash.com/${data.survey.photo}/600x300`,
     "twitter:image:alt": data.survey.text,
     "twitter:card": "summary_large_image",
     "og:url": `https://plurality.fun/surveys/${data.survey._id}/guess`,
     "og:title": `Plurality Survey #${data.survey._id}`,
-    "og:description": `Plurality Survey #${data.survey._id}`,
+    "og:description": data.survey.text,
     "og:site_name": "Plurality",
     "og:image": `https://source.unsplash.com/${data.survey.photo}/600x300`,
     "og:image:alt": data.survey.text,
