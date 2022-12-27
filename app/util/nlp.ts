06db240226a0040bd643c8dc658fd9754502b2d1
the-abe-train <abraham.train@gmail.com>
2022-12-27 10:46:54 -0500

    Fixed acronyms process

--- app/util/nlp.ts
+++ app/util/nlp.ts
@@ -1,11 +1,11 @@
 import { MongoClient } from "mongodb";
 import lemmatize from "wink-lemmatizer";
 import { votesBySurvey } from "~/db/queries";
-import { RankedVote, VoteAggregation } from "~/db/schemas";
 import { capitalizeFirstLetter } from "./text";
 import Typo from "typo-js";
 
 const spellcheckIgnore = require("../maps/spellcheck_ignore.json") as string[];
+const lemmaIgnore = require("../maps/lemma_ignore.json") as string[];
 const acronyms = require("../maps/acronyms.json") as Record<string, string>;
 
 export function getTypo(response: string) {
@@ -19,6 +19,7 @@ export function getLemma(word: string | number): string {
   if (typeof word === "number") return String(word);
   let trimmed = word.trim().toLowerCase();
   if (acronyms.hasOwnProperty(trimmed)) trimmed = acronyms[trimmed];
+  if (lemmaIgnore.includes(trimmed)) return capitalizeFirstLetter(trimmed);
   const noun = lemmatize.noun(trimmed);
   if (noun !== trimmed) return capitalizeFirstLetter(noun);
   const verb = lemmatize.verb(trimmed);
@@ -30,18 +31,21 @@ export function getLemma(word: string | number): string {
 
 export async function surveyAnswers(client: MongoClient, surveyId: number) {
   const responses = await votesBySurvey(client, surveyId);
-  const lemmatizedResponses = responses.reduce((output, response) => {
-    if (typeof response === "number") return [...output, response];
-    const lemmatized = { ...response, _id: getLemma(response._id) };
-    const existingResponse = output.find((r) => r._id === lemmatized._id);
-    if (existingResponse) {
-      const existingIdx = output.findIndex((r) => r._id === lemmatized._id);
-      existingResponse["votes"] += lemmatized.votes;
-      output.splice(existingIdx, 1, existingResponse);
-      return output;
-    }
-    return [...output, lemmatized];
-  }, [] as VoteAggregation[]);
+  const lemmatizedResponses = responses.reduce(
+    (output: VoteAggregation[], response) => {
+      if (typeof response === "number") return [...output, response];
+      const lemmatized = { ...response, _id: getLemma(response._id) };
+      const existingResponse = output.find((r) => r._id === lemmatized._id);
+      if (existingResponse) {
+        const existingIdx = output.findIndex((r) => r._id === lemmatized._id);
+        existingResponse["votes"] += lemmatized.votes;
+        output.splice(existingIdx, 1, existingResponse);
+        return output;
+      }
+      return [...output, lemmatized];
+    },
+    []
+  ) as VoteAggregation[];
 
   const rankedResponses: RankedVote[] = lemmatizedResponses
     .sort((a, z) => {
